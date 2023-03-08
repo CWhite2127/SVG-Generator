6cb6744c752593042f9789afef2561a2c152e9ad
CWhite2127 <cwhitewesterly2@gmail.com>
2023-03-07 19:07:10 -0700

    adds prompts for user

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+
+
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Please add a maximum of three characters',
+            name: 'text',
+        },
+        {
+            type: 'input',
+            message: 'Please input a text color',
+            name: 'textColor',
+        },
+        {
+            type: 'list',
+            message: 'Please choose a shape',
+            name: 'shape',
+            choices: ['circle', 'triangle', 'square'],
+        },
+        {
+            type: 'input',
+            message: 'Please insert a shape color',
+            name: 'shapeColor',
+        },
+    ])
+    .then((answers) => {
+        console.log(answers);
+    });
+
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for text
+// THEN I can enter up to three characters
+// WHEN I am prompted for the text color
+// THEN I can enter a color keyword (OR a hexadecimal number)
+// WHEN I am prompted for a shape
+// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+// WHEN I am prompted for the shape's color
+// THEN I can enter a color keyword (OR a hexadecimal number)
+// WHEN I have entered input for all the prompts
+// THEN an SVG file is created named `logo.svg`
+// AND the output text "Generated logo.svg" is printed in the command line
+// WHEN I open the `logo.svg` file in a browser
+// THEN I am shown a 300x200 pixel image that matches the criteria I entered

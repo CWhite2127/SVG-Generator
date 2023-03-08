77fe480fde71c5ecb47e232623633bb07afdbcf5
CWhite2127 <cwhitewesterly2@gmail.com>
2023-03-07 20:48:54 -0700

    adds init function and write file function

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const {writeFile} = require("fs").promises;
 
 
 
-
-inquirer
-    .prompt([
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             message: 'Please add a maximum of three characters',
@@ -28,19 +27,20 @@ inquirer
             name: 'shapeColor',
         },
     ])
-    .then((answers) => {
-        console.log(answers);
-    });
+};
+    
+// const generateSvg =({text, textColor, shape, shapeColor})
 
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
+    const init = () => {
+        promptUser()
+
+            // .then((answers) => writeFile('logo.svg', generateSvg(answers)))
+            .then(() => console.log("successfully created svg file"))
+            .catch((err) => console.log(err));
+    };
+
+    init();
+
 // WHEN I have entered input for all the prompts
 // THEN an SVG file is created named `logo.svg`
 // AND the output text "Generated logo.svg" is printed in the command line
